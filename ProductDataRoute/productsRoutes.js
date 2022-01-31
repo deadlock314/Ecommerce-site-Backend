11415b7bfa737e59de5fdcd6f0155648c78e2600
chandan soni <chandansoni2270@gmail.com>
2022-01-31 19:31:00 +0530

    restructured the / route endpoint

---
+++ ProductDataRoute/productsRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+let {ProductData,EarphoneData,MoblieData}=require('../Schema/ProductSchemas');
+
+let SchemaVar= ProductData;
+
+router.route('/').get((req,res)=>res.redirect(`/laptops`))
+
+router.route('/:products').get((req,res)=>{
+    
+    SchemaVar=(req.params.products=='mobiles')?MoblieData:(req.params.products=='earphones') ? EarphoneData : ProductData;
+
+    SchemaVar.find({},(err,doc)=>{
+        (err) ? res.end(err):res.json(doc);
+    })
+    
+});
+
+
+
+
+
+// router.route('/').post((req,res)=>{
+
+//     const NewHomeProductData= new ProductData(req.body);
+//      NewHomeProductData.save().then(res.end('hey mama'));
+    
+// });
+
+
+module.exports=router;
