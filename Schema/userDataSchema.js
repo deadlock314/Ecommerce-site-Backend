7c068e99a0ba252440e58e72dac7699fd5e65379
chandan soni <chandansoni2270@gmail.com>
2022-02-06 22:26:41 +0530

    added user data get and post endpoint

---
+++ Schema/userDataSchema.js
@@ -0,0 +1,57 @@
+const mongoose=require("mongoose");
+
+const userDataSchema = new mongoose.Schema({
+    
+    userAccData:{
+
+        userId:{
+          type:Number,
+          required:[true,'invaild userId']
+        },
+        name:{
+          type:String,
+          required:[true, 'name required'],
+          minlength:3
+
+        }, 
+        email:{
+            required:[true, 'e-mail address required'],
+            unique:[true,'existing email addresss '],
+            type:String,
+            lowercase:true
+        },
+        mobile:{
+            type:String, minlength:10
+        },  
+        orders:{
+
+            fullfilledOrders:{
+               type:Array
+            },
+            pendingOrders:{
+              type:Array
+            },
+            cartProducts:{
+              type:Array
+            }
+        },
+        
+        profession:{
+            type:String
+        }  
+    }
+    ,
+    userNonImpData:{
+        
+       AccCreationDate:{
+         type:Date
+
+       } 
+    }
+    
+    
+
+        
+});
+const User=mongoose.model('user',userDataSchema)
+module.exports=User;
---
+++ UserDataRoute/userGetRoute.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const authTesterMiddleware=require('../middleware/authTesterMid');
+const User =require('../Schema/UserSchema');
+
+router.route('/user/:id').get(authTesterMiddleware,(req,res)=>{
+    try{
+        const doc= await User.findOne({userId:req.id})
+        res.json(doc);
+    }
+    catch(err){
+        res.status(400).json({isSuccess:false});
+    }
+})
+
+router.route('/user/:id').post(authTesterMiddleware,(req,res)=>{
+    try{
+        const doc= await User.findOneAndUpdate({userId:req.id},req.body)
+        res.status(200).json({isUpdatetd:false});
+    }
+    catch(err){
+        res.status(400).json({isUpdatetd:false});
+    }
+})
+
+
+
+module.exports=router;
