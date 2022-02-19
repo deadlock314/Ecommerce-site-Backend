10fca10baa003daa442786412733a2bf113462a1
Chandan Soni <71438903+deadlock314@users.noreply.github.com>
2022-02-19 23:12:52 +0530

    changed status code

--- authRoute/signUpRoute.js
+++ authRoute/signUpRoute.js
@@ -9,12 +9,12 @@ const users=req.body;
 
      userAuth.findOne({email:users.email}).then((doc,err)=>{
         if(doc){
-            res.status(400).json({isUserSignedUp:false,isDuplicateUser:true,isEmailSent:false});
+            res.json({isUserSignedUp:false,isDuplicateUser:true,isEmailSent:false});
         }
         else {
             newTempAuthUser.save((err,result)=>{
                 if(err)
-                res.status(400).json({isUserSignedUp:false,isDuplicateUser:true,isEmailSent:false})
+                res.json({isUserSignedUp:false,isDuplicateUser:true,isEmailSent:false})
                 else{
                    createMailSender();
                     const mailRes =mailSenderFun(result.email,"verifying sign Up mail",{otp:result.otp,name:users.name});
