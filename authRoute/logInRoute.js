a84be58d26c6c3c0975e1532bed8949837afd85d
chandan soni <chandansoni2270@gmail.com>
2022-02-23 20:14:07 +0530

    added more info of err  in Login Route

--- authRoute/logInRoute.js
+++ authRoute/logInRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const userAuth =require('../Schema/userAuthSchema');
+const {userAuth} =require('../Schema/userAuthSchema');
 const jwt =require('jsonwebtoken');
 
 router.route('/').post( (req,res)=>{
@@ -9,24 +9,28 @@ router.route('/').post( (req,res)=>{
     
             userAuth.findOne({email:logUser.email}).then((doc,err)=>{
                 if(err)
-                    res.status(400).json({isUserLoggedIn:false})
-               else{
+                    res.status(400).json({isUserLoggedIn:false,isCorrectPassword:false,isNetworkError:true});
+                else if(doc)
+                {
                     const tokenData={doc,date: new Date() }
                     //console.log(doc)
                     bcrypt.compare(logUser.password, doc.password, (err, result)=> {
                         if(result){
                             const token= jwt.sign(tokenData, process.env.SECRECT);
-                            res.cookie("auth",token ,{maxAge:1000*60*600,sameSite:'none',secure:true}).json({isUserLoggedIn:true,id:doc.userId});
+                            res.cookie("auth",token ,{maxAge:1000*60*600,sameSite:'none',secure:true}).json
+                            ({isUserLoggedIn:true,isCorrectPassword:false,isCorrectUser:true});
                         }
                         else
-                            res.status(400).json({isUserLoggedIn:false})
+                            res.status(400).json({isUserLoggedIn:false,isCorrectPassword:false});
                    
                     })
-               }
+                } 
+                else
+                res.status(400).json({isUserLoggedIn:false,isCorrectPassword:false,isCorrectUser:false})
                 
         }).catch((err)=>{
                 console.log(err)
-                res.status(400).json({isUserLoggedIn:false})
+                res.status(400).json({isUserLoggedIn:false,isNetworkError:true})
             })
    
 })
