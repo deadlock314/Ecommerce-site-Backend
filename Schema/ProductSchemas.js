fb624402a4323a69eecb76c6efcb430d64596e92
chandan soni <chandansoni2270@gmail.com>
2022-03-08 10:56:04 +0530

    generalised product schemas

--- Schema/ProductSchemas.js
+++ Schema/ProductSchemas.js
@@ -1,42 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const productSchema= new mongoose.Schema({
-    productId:{
-        type:String, required:true, unique:true
-    },
-    imgLink:{
-        type:String
-        
-    },
-    des:{
-        type:String, required:true
-    },
-    price:{
-        type:String, required:true
-    }
+const productArr=['laptops','earphones','mobiles','smartwatches'];
+
+let SchemaObj={},ExdesSchemaObj={};
+
+const schemaPropObj= new mongoose.Schema({
+    
+    productId: {   type: String,  },
+    imgLink: { type: String },
+    des: { type: String },
+    price: {  type: String }
 
 });
+const exdesSchemaPropObj= new mongoose.Schema({
 
-const productExdesSchema= new mongoose.Schema({
-     productId:{
-        type:String, required:true, unique:true
-    },
-    extendedDes:{
-        type:Object, required:true,
-        General:{
-            type:Array,
-            required:true
+    productId: {  type: String,  unique: true  },
+    extendedDes: {  type: Object, 
+        General: {
+            type: Array,
+            required: true
         }
-
     }
-})
+});
 
-const ProductData=mongoose.model('homeProductData',productSchema);
-const MoblieData=mongoose.model('MoblieData',productSchema);
-const EarphoneData=mongoose.model('EarphoneData',productSchema);
 
-const ProductDataExdes=mongoose.model('ProductDataExdes',productExdesSchema);
-const MoblieExdesData=mongoose.model('MoblieExdesData',productExdesSchema);
-const EarphoneExdesData=mongoose.model('EarphoneExdesData',productExdesSchema);
+ for (let i = 0; i <productArr.length; i++){
+     SchemaObj[productArr[i]]=[schemaPropObj]
+     ExdesSchemaObj[productArr[i]]=[exdesSchemaPropObj];
+ }
+      
 
-module.exports={ProductData ,ProductDataExdes ,EarphoneData,MoblieData ,EarphoneExdesData,MoblieExdesData};
+const productSchema = new mongoose.Schema({...SchemaObj });
+const productExdesSchema = new mongoose.Schema({ ...ExdesSchemaObj })
+
+const ProductData = mongoose.model('ProductData', productSchema);
+const ProductDataExdes = mongoose.model('ProductDataExdes', productExdesSchema);
+
+module.exports = { ProductData, ProductDataExdes };
